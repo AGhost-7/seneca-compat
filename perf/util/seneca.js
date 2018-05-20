8cbe55f1e4c6cb759223b9578bf2acee06aa7ea9
AGhost-7 <jonathan.boudreau.92@gmail.com>
2018-05-20 15:35:55 -0400

    Add submodule to make a/b tests easier

--- perf/util/seneca.js
+++ perf/util/seneca.js
@@ -1,5 +1,6 @@
 
-module.exports = () => require('../../seneca')({
+var senecaPath = process.env.SENECA_PATH || '../../seneca.js';
+module.exports = () => require(senecaPath)({
 	log: 'silent',
 	actcache: false,
 	timeout: 30000,
